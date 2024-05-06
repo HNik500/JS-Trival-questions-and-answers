fe7c04ead56235920d1c8422a62268b3bd0ae307
Nikhil Hazari <nikhilhazari100@gmail.com>
2024-05-06 20:15:47 +0100

    added some more info

--- js.js
+++ js.js
@@ -26,6 +26,7 @@ function testing(arr){
      console.log(arr.length);
 }
 testing(azzaz)
+
 // Capitalize Words: Implement a function that capitalizes the first letter of each word in a sentence.
 function captilize(arr){
     let store1 = arr.split(" ");
@@ -36,7 +37,21 @@ for(let i = 0; i<store1.length; i++){
 return console.log ( store1.join(" ") );
 }
 captilize("messi is the best player in the world")
-// to get the biggest no
+// alternate to capatalize
+// function captilize(arr){
+//     let store1 = arr.split(" ");
+
+// for(let i = 0; i<store1.length; i++){
+//     store1[i] = store1[i][0].toUpperCase() + store1[i].slice(1);
+     
+//     // console.log(store1[i][0].toUpperCase()) 
+// } 
+// return console.log ( store1.join(" ") );
+// }
+// captilize("messi is the best player in the world")
+
+
+//to get the biggest no
 function findMax(arr){
     let max = arr[0]; // 10 
 for(let i = 1 ; i<arr.length; i++){
@@ -75,3 +90,54 @@ return console.log ( store )
 }
 let arrax = [10,20,30,40,50];
 sumofarray(arrax);
+
+// Filter Array
+let myarray = [-2,-1,0,1,20];
+function filterfun(a,i){
+    return a>10;
+}
+
+function mainfun(arr,fun){
+let emptyarray =[];
+for(let i =0; i<arr.length; i++){
+    if (fun (arr[i],i )){
+    emptyarray.push(   arr[i] ) 
+}
+
+}
+return emptyarray;
+}
+console.log ( mainfun(myarray, filterfun) )
+
+
+// factorial
+function factorial(a){
+    let b = 1;
+    for(let i = a; i >  1; i--){
+        b*=a;
+    }
+    return b;
+}
+console.log ( factorial(3) );
+
+//prime numbers
+function checkprime(n){
+    if(n % 2 || n % 3 || n % 7 == 0){
+console.log(" prime number")
+    } else {
+        console.log("not prime number")
+    }
+ }
+checkprime(5);
+
+// prime no 2
+// function checkprime2(n){
+//     if(n >= 1 ){
+//     for(let i = 2; i < n; i++ ){
+//         if(n % i === 0 ){
+//            return "not a prime number"
+//         } else   return "prime no! " 
+//     }
+//     }
+// }
+// console.log (  checkprime2(3) ) 
